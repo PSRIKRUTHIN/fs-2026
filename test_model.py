80ede762e8cc7cc7da811fbd1a8264990f091efc
KRUTHIN <psrikruthin@gmail.com>
2026-03-11 14:54:00 +0530

    deploy added-3

--- test_model.py
+++ test_model.py
@@ -1,8 +1,8 @@
 import numpy as np
-from sklearn.model_selection import LinearRegression
+from sklearn.linear_model import LinearRegression
 
 X = np.array([[1],[2],[3],[4]])
-y = np.array([[1,4,6,9]])
+y = np.array([1,4,6,9])
 
 def test_coefficients():
     model = LinearRegression()
@@ -17,5 +17,5 @@ def test_intercept():
 def test_predictions():
     model = LinearRegression()
     model.fit(X, y)
-    predictions = model.predict(X)
+    pred = model.predict(X)
     assert round(pred[0],2) ==10.0
